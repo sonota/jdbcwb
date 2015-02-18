1692637f282a24b9d8da2c0d46b59cb2102bfe75
sonota88 <yosiot8753@gmail.com>
2015-02-19 05:11:32 +0900

    implement query

--- app.js
+++ app.js
@@ -1,6 +1,114 @@
 var app = Kijitora.App.create(APP_ROOT);
 
+var FETCH_LIMIT = 100;
+
+var canScroll;
+
 ////////////////////////////////
+
+var getConnectionFuncs = {
+  mysql: function(url, props){
+    return new Packages.com.mysql.jdbc.Driver().connect(
+      url, props);
+  }
+};
+
+function getConnection(){
+  if( GLOBAL.conn && ! GLOBAL.conn.isClosed() ){
+    return GLOBAL.conn;
+  }
+
+  var config = GLOBAL.config.db;
+
+  puts_debug(JSON.stringify(config));
+  var _props = new java.util.Properties();
+  for(var k in config.props){
+    _props.put(k, config.props[k]);
+  }
+
+  if(config.type === "mysql"){
+    GLOBAL.conn = getConnectionFuncs[GLOBAL.config.db.type](
+      config.url, _props);
+  }else{
+    throw new Error("config.db.type is invalid (" + config.db.type + ")");
+  }
+
+  // java.sql.DatabaseMetaData
+  var dbmd = GLOBAL.conn.getMetaData();
+  putskv("JDBC major version", dbmd.getJDBCMajorVersion());
+
+  canScroll = dbmd.supportsResultSetType(
+    java.sql.ResultSet.TYPE_SCROLL_INSENSITIVE);
+  putskv("ResultSet.TYPE_SCROLL_INSENSITIVE", canScroll);
+
+  return GLOBAL.conn;
+}
+
+
+function getMetaData(rs){
+  var md = rs.getMetaData();
+  var numCols = md.getColumnCount();
+  puts_debug("numCols=" + numCols);
+  var mds = [];
+  for(var i=0; i<numCols; i++){
+    var n = i + 1;
+    mds.push({
+      no: n,
+      name: "" + md.getColumnLabel(n),
+      type: "" + md.getColumnTypeName(n),
+      pk: null
+    });
+  }
+  return mds;
+}
+
+////////////////////////////////
+
+var Record = (function(){
+
+  function Record(){
+    this.cols = [];
+  };
+  var __ = Record.prototype;
+
+  Record.fromRs = function(rs, md){
+    var numCols = md.length;
+    var rec = new Record();
+    for(var i=0; i<numCols; i++){
+      var val = rs.getObject(i + 1);
+      if(val === null){
+        rec.set(i, null);
+      }else if( md[i].type === "INTEGER" ){
+        rec.set(i, parseInt("" + val, 10));
+      }else{
+        rec.set(i, "" + val);
+      }
+    }
+    return rec;
+  };
+
+  __.set = function(i, val){
+    this.cols[i] = val;
+  };
+
+  return Record;
+})();
+
+function rs2recs(rs, md, limit){
+  var recs = [];
+  var i = 0;
+  while(rs.next() && i < limit){
+    recs.push(Record.fromRs(rs, md));
+    i++;
+  }
+  return recs;
+}
+
+////////////////////////////////
+
+function date2str(date){
+  return "" + date.getTime() + "_" + date;
+}
 
 function getView(name){
   var path = APP_ROOT + "/views/" + name + ".html";
@@ -34,15 +142,78 @@ app.get("/jdbcwb", function(req, res){
   res.send(getView("main"));
 });
 
+////////////////////////////////
+
+function query(conn, sql, params){
+  var stmt;
+  if(canScroll){
+    stmt = conn.createStatement(
+      java.sql.ResultSet.TYPE_SCROLL_INSENSITIVE,
+      java.sql.ResultSet.CONCUR_READ_ONLY
+    );
+  }else{
+    stmt = conn.createStatement();
+  }
+
+  var execTime = new Date();
+  var rs = stmt.executeQuery(sql);
+
+  // 全体（limitなし）の件数を取得
+  var numRowsAll = null;
+  if(canScroll){
+    rs.last();
+    numRowsAll = rs.getRow();
+    rs.beforeFirst();
+  }
+
+  var md = getMetaData(rs);
+  var recs = rs2recs(rs, md, FETCH_LIMIT);
+  puts_debug("" + recs.length + " rows");
+
+  rs.close();
+  stmt.close();
+
+  puts_debug("mode=" + params.mode);
+  var colDefs;
+  if(params.mode === 'single_table'){
+    colDefs = getColDefsForTable(
+      conn, params.schema, params.table);
+  }else{
+    colDefs = md;
+  }
+
+  return {
+    sql: sql,
+    rows: _.map(recs, function(rec){
+      return rec.cols;
+    }),
+    numRows: recs.length,
+    numRowsAll: numRowsAll, // limit なしの件数
+    timestamp: date2str(execTime),
+    colDefs: colDefs
+  };
+}
+
+
 app.post("/api/query", function(req, res){
   return _api(req, res, function(req, res){
-    // TODO
+
+    var sqls = JSON.parse(req.params.sqls);
+
+    var conn = getConnection();
+
+    var results = _.map(sqls, function(sql){
+      return query(conn, sql, req.params);
+    });
+
     return {
       status: "OK",
-      results: []
+      results: results
     };
   });
 });
+
+////////////////////////////////
 
 app.post("/api/update", function(req, res){
   return _api(req, res, function(req, res){
