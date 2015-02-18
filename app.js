b5ba2ea64bf8bc25d866299d31637a7cc3bb8cd8
sonota88 <yosiot8753@gmail.com>
2015-02-19 04:04:54 +0900

    add _api()

--- app.js
+++ app.js
@@ -7,10 +7,50 @@ function getView(name){
   return _File.read(path);
 }
 
+function _api(req, res, fn){
+  res.setContentType("application/json");
+  var data;
+  try{
+    data = fn(req, res);
+  } catch (ex) {
+    dump(ex);
+    data = {
+      status: "NG",
+      ex: {
+        message: ex.message,
+        name: ex.name,
+        fileName: ex.fileName,
+        lineNumber: ex.lineNumber
+      }
+    };
+  }
+  var json = JSON.stringify(data);
+  res.send(json);
+}
+
 ////////////////////////////////
 
 app.get("/jdbcwb", function(req, res){
   res.send(getView("main"));
+});
+
+app.post("/api/query", function(req, res){
+  return _api(req, res, function(req, res){
+    // TODO
+    return {
+      status: "OK",
+      results: []
+    };
+  });
+});
+
+app.post("/api/update", function(req, res){
+  return _api(req, res, function(req, res){
+    // TODO
+    return {
+      status: "OK"
+    };
+  });
 });
 
 exports.app = app;
